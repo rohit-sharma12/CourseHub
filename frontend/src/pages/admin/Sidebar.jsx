0037210ddc8ec92cadc0ba8b37d8448174d778da
rohitsharma <rs0043071@gmail.com>
2026-02-16 10:03:59 +0530

    creating edit functionality

--- frontend/src/pages/admin/Sidebar.jsx
+++ frontend/src/pages/admin/Sidebar.jsx
@@ -3,10 +3,9 @@ import { Link, Outlet } from "react-router-dom";
 
 const Sidebar = () => {
     return (
-        <div className="flex min-h-screen">
-            {/* Sidebar */}
-            <div className="hidden lg:block w-[250px] border-r border-gray-300 dark:border-gray-700 bg-[#f0f0f0] p-5">
-                <div className="space-y-6">
+        <div className="flex">
+            <div className="hidden lg:block w-[250px] sm:w-[300px] space-y-8 h-screen border-r border-gray-300 dark:border-gray-700 bg-[#f0f0f0] sticky top-0 p-5">
+                <div className="space-y-4">
                     <Link to="/admin/dashboard" className="flex items-center gap-2">
                         <ChartNoAxesColumn size={22} />
                         <h1>Dashboard</h1>
@@ -21,7 +20,7 @@ const Sidebar = () => {
             </div>
 
             {/* Main Content */}
-            <div className="flex-1 p-6 bg-white">
+            <div className="flex-1 md:p-24 p-2 bg-white">
                 <Outlet />
             </div>
         </div>
--- frontend/src/pages/admin/course/CourseTab.jsx
+++ frontend/src/pages/admin/course/CourseTab.jsx
@@ -1,9 +1,27 @@
 import { Card, CardHeader, CardDescription, CardTitle, CardContent } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
+import RichTextEditor from "../../../components/RichTextEditor";
+import { useState } from "react";
+import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from "@/components/ui/select"
+import { Loader2 } from "lucide-react";
 
 const CourseTab = () => {
+    const [input, setInput] = useState({
+        courseTitle: "",
+        subTitle: "",
+        description: '',
+        category: "",
+        courseLevel: "",
+        coursePrice: "",
+        courseThumbnail: ""
+    });
+    const changeEventHandler = (e) => {
+        const { name, value } = e.target;
+        setInput({ ...input, [name]: value })
+    }
     const isPublished = true;
+    const isLoading = false;
 
     return (
         <Card>
@@ -28,25 +46,80 @@ const CourseTab = () => {
                 <div className="space-y-4 mt-5">
                     <div>
                         <Label>Title</Label>
-                        <input type="text" placeholder="Ex. Fullstack Developer" name="courseTitle" />
+                        <input type="text" value={input.courseTitle} onChange={changeEventHandler} placeholder="Ex. Fullstack Developer" name="courseTitle" />
                     </div>
                 </div>
                 <div className="space-y-4 mt-5">
                     <div>
                         <Label>Subtitle</Label>
-                        <input type="text" placeholder="Ex. Fullstack Developer" name="courseTitle" />
-                    </div>
-                </div>
-                <div className="space-y-4 mt-5">
-                    <div>
-                        <Label>Subtitle</Label>
-                        <input type="text" placeholder="Ex. Become a Fullstack Developer" name="subTitle" />
+                        <input type="text" value={input.subTitle} onChange={changeEventHandler} placeholder="Ex. Fullstack Developer" name="subTitle" />
                     </div>
                 </div>
                 <div className="space-y-4 mt-5">
                     <div>
                         <Label>Description</Label>
-                        <RichTextEditor />
+                        <RichTextEditor input={input} setInput={setInput} />
+                    </div>
+                    <div className="flex items-center gap-5">
+                        <div>
+                            <Label>Category</Label>
+                            <Select>
+                                <SelectTrigger className="w-full max-w-48">
+                                    <SelectValue placeholder="Select a Category" />
+                                </SelectTrigger>
+                                <SelectContent>
+                                    <SelectGroup>
+                                        <SelectLabel>Category</SelectLabel>
+                                        <SelectItem value="Next JS"></SelectItem>
+                                        <SelectItem value="MERN Stack">MERN Stack</SelectItem>
+                                        <SelectItem value="HTML">HTML</SelectItem>
+                                        <SelectItem value="mobile">Mobile Developemnt</SelectItem>
+                                        <SelectItem value="springboot">SpringBoot</SelectItem>
+                                        <SelectItem value="AI/ML">AI/ML</SelectItem>
+                                    </SelectGroup>
+                                </SelectContent>
+                            </Select>
+                        </div>
+                        <div>
+                            <Label>Course Level</Label>
+                            <Select>
+                                <SelectTrigger className="w-full max-w-48">
+                                    <SelectValue placeholder="Select a Category" />
+                                </SelectTrigger>
+                                <SelectContent>
+                                    <SelectGroup>
+                                        <SelectLabel>Course Level</SelectLabel>
+                                        <SelectItem value="Beginner">Beginner</SelectItem>
+                                        <SelectItem value="Medium">Medium</SelectItem>
+                                        <SelectItem value="Advance">Advance</SelectItem>
+
+                                    </SelectGroup>
+                                </SelectContent>
+                            </Select>
+                        </div>
+                        <div>
+                            <Label>Price</Label>
+                            <input type="number" name="coursePrice" value={input.coursePrice} onChange={changeEventHandler} placeholder="999" className="w-full" />
+                        </div>
+
+                    </div>
+                    <div>
+                        <Label>Course Thumbnail</Label>
+                        <input type="file" accept="image/*" className="w-fit" />
+                    </div>
+                    <div>
+                        <Button variant="outline">Cancel</Button>
+                        <Button disabled={isLoading}>
+                        {
+                            isLoading ? (
+                                <>
+                                    <Loader2 className="mr-2 h-4 w-4 animate-spin" /> Please wait
+                                </>
+                            ) : (
+                                "Save"
+                            )
+                            }
+                        </Button>
                     </div>
                 </div>
 
